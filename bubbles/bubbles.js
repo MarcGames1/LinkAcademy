241bcad6cdcf730e191bf0c79835e8605e588ab7
marcgames1 <alex@DESKTOP-VHVLO28.localdomain>
2021-02-13 18:12:40 +0200

    curs 6

--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -6,13 +6,23 @@ for (let i = 0; i < numberOfBubles; i++) {
 
 function newBuble() {
   let bubble = document.createElement('div');
-  bubble.classList.add('bubble');
   let x = randomNumber(100);
-  let delay = randomNumber(1000);
-  bubble.style.left = x + 'vw';
-  bubble.style.animationDelay = delay + 'ms';
+  let delay = randomNumber(5000);
+  let blue = randomNumber(255)
+  let green = randomNumber(255)
+  let red = randomNumber(255)
+  let size = randomNumber(3) + 1
+  let shadowColor = 10
+  bubble.classList.add('bubble');
+  bubble.style.height = bubble.style.width = `${size}em`
+  bubble.style.left = `${x}vw`;
+  bubble.style.animationDelay = `${delay}ms`;
+  bubble.style.borderColor = `rgb(${red}, ${green}, ${blue})`
+  bubble.style.boxShadow = `0.1em 0.1em 0.1em 0.1em 
+  rgb(${red -shadowColor}, ${green -shadowColor}, ${blue -shadowColor}) inset`
   document.querySelector('body').appendChild(bubble);
 }
+  
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
 }
